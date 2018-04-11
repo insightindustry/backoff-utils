b08e2f555d561159dc5e880dd67957aeebd1a7f6
Chris Modzelewski <chris.modzelewski@insightindustry.com>
2018-04-11 12:22:29 -0400

    Created backoff_utils/__init__.py

---
+++ backoff_utils/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Backoff-Utils
+#########################
+
+A collection of Python functions, decorators, and context managers that can be
+used to simply and efficiently apply backoff and retry strategies in your Python
+code.
+
+"""
+
+__all__ = [
+
+]
