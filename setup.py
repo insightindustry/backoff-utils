9de7356be6e115739aecb0a59e6fcdce3b975161
insightindustry <software@insightindustry.com>
2018-04-19 12:25:51 -0400

    Revised version number management.

--- setup.py
+++ setup.py
@@ -14,8 +14,15 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'docs/index.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+# Get the version number from the VERSION file
+version_dict = {}
+with open('./validator_collection/_version.py') as version_file:
+    exec(version_file.read(), version_dict)                                     # pylint: disable=W0122
+
+version = version_dict.get('__version__')
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
@@ -40,7 +47,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',  # Required
+    version=version,  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -83,7 +90,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
@@ -94,6 +101,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
@@ -138,7 +147,7 @@ setup(
         'test': ['coverage', 'pytest','pytest-benchmark','pytest-cov','tox','codecov'],
     },
 
-    python_requires='!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.
@@ -181,8 +190,6 @@ setup(
     project_urls={  # Optional
         'Documentation': 'http://backoff-utils.readthedocs.io/en/',
         'Bug Reports': 'https://github.com/insightindustry/backoff-utils/issues',
-        'Funding': 'https://donate.pypi.org',
-        'Say Thanks!': 'http://saythanks.io/to/example',
         'Source': 'https://github.com/insightindustry/backoff-utils/',
     },
 )
